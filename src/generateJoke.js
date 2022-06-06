5b101d1fadb87b345b723d2e6b22620e97c13716
Pritesh Kakani <priteshgkakani@gmail.com>
2022-06-06 19:05:40 +0530

    basic code

---
+++ src/generateJoke.js
@@ -0,0 +1,5 @@
+function generateJoke(){
+    return "Should you find yourself in a chronically leaking boat, energy devoted to changing vessels is likely to be more productive than energy devoted to patching leaks - Warren Buffett";
+}
+
+export default generateJoke
